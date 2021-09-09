f86a8b13050f0d94eebe9d0a88c9eb9781c9ecf3
Abel <abelvanhulst@gmail.com>
2021-09-09 19:43:14 +0200

    5️⃣readme en een setup gemaakt

---
+++ README.md
@@ -0,0 +1,4 @@
+#parel assistant
+Hoi
+Dit is de beste nederlandse assistant 😁
+Het is nog steeds in dev
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+#import random
+#import time
+#import turtle as t
+#import webbrowser as web
+#import string
+#from pprint import pprint
+#import os
+#import tkinter as tk
+#from tkinter import simpledialog as simpel
+
+print("Alle pakketten installeren")
+
+os.system("pip install random")
+os.system("pip install time")
+os.system("pip install turtle")
+os.system("pip install webbrowser")
+os.system("pip install string")
+os.system("pip install pprint")
+os.system("pip install os")
+os.system("pip install tkinter")
+
+print("Pakketten geinstalleerd!")
