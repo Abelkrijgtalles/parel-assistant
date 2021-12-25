cb478f00080135d34494b85d3436dabbe7b4ed6d
Abel van Hulst <abelvanhulst@gmail.com>
2021-12-25 15:34:13 +0100

    werkt ie nu?

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /home/parel-assistant
 COPY . /home/parel-assistant
 RUN cd /home/parel-assistant
 RUN pip install transcrypt
+RUN python3 aalleenvoorgithubtesten/vinddeimports.py
 RUN transcrypt main.py
---
+++ alleenvoorgithubtesten/vinddeimports.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+os.system("cp " + os.__file__ + "./os.py")
+os.system("cp " + json.__file__ + "./json.py")
+os.system("pip install turtle")
+import turtle
+os.system("cp " + turtle.__file__ + "./turtle.py")
+import random
+os.system("cp " + random.__file__ + "./random.py")
+import tkinter
+os.system("cp " + tkinter.__file__ + "./os.py")
+import webbrowser
+os.system("cp " + webbrowser.__file__ + "./webbrowser.py")
+import pickle
+os.system("cp " + pickle.__file__ + "./pickle.py")
