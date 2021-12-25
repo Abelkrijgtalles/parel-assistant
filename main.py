b14584893c6758ae00b649a6ba39b687d85cd4c0
Abel van Hulst <abelvanhulst@gmail.com>
2021-12-25 14:17:27 +0100

    godzamme werk nou

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import ding
+def main():
+    import os
+    from openeentranslate import openen as op
+    from openlandcode import openen as landc
+    from parelassistantding import main as startem
+    # import random
+    # import time
+    # import turtle as t
+    # import webbrowser as web
+    # import string
+    # from pprint import pprint
+    # import os
+    # import tkinter as tk
+    # from tkinter import simpledialog as simpel
 
-ding.main()
+    taal = landc()
+
+    with open(os.path.join("config", "welketaal")) as f:
+        welketaal = input(op(taal, "welketaal"))
+        f.write(welketaal)
+
+    print(op(taal, "prostart"))
+    startem()
