baf78279260c39195e4cb92b5f70b9a9d7a06b73
Abel van Hulst <abelvanhulst@gmail.com>
2021-12-25 14:40:45 +0100

    WERK!

---
+++ commands/__init__.py
@@ -0,0 +1,7 @@
+import commands.ALLESONGTEKSTENNL as songteksten
+import commands.DATA as data
+import commands.FEITEN as feiten
+import commands.GAGILLEN as gillen
+import commands.LANDEN as landen
+import commands.PAASDAG as paasdag
+import commands.TEKENCORONA as tekencorona
